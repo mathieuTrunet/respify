a28d804a78e8af51a06f48bed53fc9b5727dbdb0
mathieuTrunet <mathieu1508@gmail.com>
2025-01-13 16:28:54 +0100

    feature: added breakpoint display on tooltip

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -12,7 +12,7 @@ const checkTailwind = () => {
   if (hasTailwindCdn) return true
 
   const hasTailwindStylesheet = Array.from(document.styleSheets).some(sheet =>
-    Array.from(sheet.cssRules).some(rule => rule.cssText.includes('--tw'))
+    Array.from(sheet.cssRules).some(rule => rule.cssText.includes('--tw-'))
   )
 
   if (hasTailwindStylesheet) return true
@@ -20,22 +20,57 @@ const checkTailwind = () => {
   return false
 }
 
+const getBreakpointValue = () => {
+  screenWidth = window.screen.width
+
+  if (screenWidth >= 1536) return '2xl'
+
+  if (screenWidth >= 1280) return 'xl'
+
+  if (screenWidth >= 1024) return 'lg'
+
+  if (screenWidth >= 768) return 'md'
+
+  if (screenWidth >= 640) return 'sm'
+
+  return 'xs'
+}
+
+const generateElement = element => {
+  element.className = 'respify'
+  element.style.zIndex = 9999
+  element.style.position = 'fixed'
+
+  element.textContent = `breakpoint: ${getBreakpointValue()}`
+  element.style.bottom = '0'
+  element.style.right = '0'
+
+  element.style.display = 'inline-flex'
+  element.style.paddingTop = '0.125rem'
+  element.style.paddingBottom = '0.125rem'
+  element.style.paddingLeft = '0.625rem'
+  element.style.paddingRight = '0.625rem'
+  element.style.margin = '0.5rem'
+  element.style.alignItems = 'center'
+  element.style.backgroundImage =
+    'linear-gradient(to left top, rgb(254, 249, 195), rgb(253, 224, 71), rgb(234, 179, 8))'
+  element.style.borderRadius = '9999px'
+  element.style.borderWidth = '1px'
+  element.style.fontSize = '1.25rem'
+  element.style.lineHeight = '1rem'
+  element.style.fontWeight = 600
+  element.style.borderColor = 'black'
+  element.style.color = 'black'
+
+  document.body.appendChild(element)
+
+  setInterval(() => (element.textContent = `breakpoint: ${getBreakpointValue()}`), 50)
+}
+
 const hasTailwind = checkTailwind()
 
 if (hasTailwind) {
   const element = document.createElement('div')
-  element.style.backgroundColor = 'red'
-  element.textContent = `dfgs`
-  element.style.position = 'fixed'
-  element.style.top = '0'
-  element.className = 'respify'
 
-  if (true) {
-    element.style.top = '1000'
-  }
-
-  element.style.left = '0'
-  element.style.zIndex = 9999
-
-  document.body.appendChild(element)
+  generateElement(element)
 }
